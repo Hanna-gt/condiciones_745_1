6c629dbbb9e181e5a9360e5fcdeef4ae7af1ae47
Hanna-gt <hgarciat@udistrital.edu.co>
2024-08-23 13:53:26 -0500

    llegue hasta el if y el else (paso 1) para calcular la edad/pendente a probar

--- Condicion.java
+++ Condicion.java
@@ -3,18 +3,40 @@ import java.util.Scanner;
 class Condicion {
   public static void main(String[] args) {
     Scanner leer = new Scanner(System.in);
-    System.out.println("Ingrese un numero");
-    int n1 = leer.nextInt();
-    System.out.println("Ingrese otro numero");
-    int n2 = leer.nextInt();
+String name;
+int mesa=8;
+int mesu;
+int anoa=2024;
+int anou;
+int diaa=23;
+int diau;
+int edad1;  
+int edad2;
+  
+System.out.println("escriba su nombre");
+    name = leer.nextInt();
+   
 
-    if(n1>n2)
+ System.out.println(name+"Ingrese su año de nacimiento");
+    anou = leer.nextInt();
+    System.out.println(name+"Ingrese el numero de su mes de nacimiento");
+    mesu = leer.nextInt();
+
+ if(mesu<=mesa)
     {
-      System.out.println("El numero: "+n1+" es el mayor");
+      System.out.println(name+ "su edad es:" );
+      System.out.println(edad1=anoa-anou);
     }
+
     else
     {
-      System.out.println("El numero: "+n2+" es el mayor");
+     System.out.println(name+ "su edad es:" );
+      System.out.println(edad2=edad1-1);
     }
-  }
+
 }
+}
+
+
+
+//nombre. año y mes de nacimiento. calcular edad y luego dia de nacimiento. dar signo//
